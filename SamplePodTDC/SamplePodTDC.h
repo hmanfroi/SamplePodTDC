e6934d9e2750335bb63bd7756f6b2af0c07a04be
Henrique Manfroi <hmanfroi@hotmail.com>
2019-11-10 10:27:56 -0300

    Start Lib

---
+++ SamplePodTDC/SamplePodTDC.h
@@ -0,0 +1,19 @@
+//
+//  SamplePodTDC.h
+//  SamplePodTDC
+//
+//  Created by Henrique Manfroi da Silveira on 10/11/19.
+//  Copyright © 2019 Henrique Manfroi da Silveira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SamplePodTDC.
+FOUNDATION_EXPORT double SamplePodTDCVersionNumber;
+
+//! Project version string for SamplePodTDC.
+FOUNDATION_EXPORT const unsigned char SamplePodTDCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamplePodTDC/PublicHeader.h>
+
+
