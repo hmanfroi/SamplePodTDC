d12768bc06db2082ffc9a1e7ee6dd3eaa3424b39
Henrique Manfroi <hmanfroi@hotmail.com>
2019-11-10 14:01:57 -0300

    VC Test and Run Start

---
+++ SamplePodTDC/Core/Presentation/Presentation.swift
@@ -0,0 +1,17 @@
+//
+//  Presentation.swift
+//  SamplePodTDC
+//
+//  Created by Henrique Manfroi da Silveira on 10/11/19.
+//  Copyright © 2019 Henrique Manfroi da Silveira. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Struct
+
+public struct Presentation {
+    
+    var title: String = ""
+    var authors: [String] = []
+}
---
+++ SamplePodTDC/Core/Presentation/PresentationViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  PresentationViewModel.swift
+//  SamplePodTDC
+//
+//  Created by Henrique Manfroi da Silveira on 10/11/19.
+//  Copyright © 2019 Henrique Manfroi da Silveira. All rights reserved.
+//
+
+// MARK: - Class
+
+public class PresentationViewModel {
+    
+    // MARK: Internal variables
+    
+    let presentation: Presentation
+    
+    // MARK: Initializers
+    
+    init(presentation: Presentation) {
+        self.presentation = presentation
+    }
+    
+    // MARK: Internal methods
+    
+    func title() -> String {
+        self.presentation.title
+    }
+    
+    func authors() -> [String] {
+        self.presentation.authors
+    }
+}
